f25747b08f23e5702e4354f475bbceb64847e806
Michał Lupa <michal.lupa4@wp.pl>
2020-04-05 23:11:27 +0200

    dzialania na wek i mac

--- inc/Macierz.hh
+++ inc/Macierz.hh
@@ -15,6 +15,12 @@ public:
     Wektor operator[] (int i) const;
     Wektor& operator[] (int i);
 
+    double Wyznacznik() const;
+    void Transpozycja(Macierz & Mc);
+
+    const Macierz  operator + (const Macierz & W) const;
+    const Macierz  operator - (const Macierz & W) const;
+    const Macierz  operator * (const Macierz & W) const;
 };
 
 std::istream& operator >> (std::istream & Strm, Macierz & Mac);
--- src/Macierz.cpp
+++ src/Macierz.cpp
@@ -14,6 +14,62 @@ Wektor & Macierz::operator[] (int i)
     return Tab[i];
 }
 
+double Macierz::Wyznacznik() const
+{
+    double sp_sum = 0;
+    double sm_sum = 0;
+    sp_sum = (Tab[0])[0] * (Tab[1])[1] * (Tab[2])[2] + (Tab[0])[1] * (Tab[1])[2] * (Tab[2])[0] + (Tab[0])[2] * (Tab[1])[0] * (Tab[2])[1];
+    sm_sum = (Tab[2])[0] * (Tab[1])[1] * (Tab[0])[2] + (Tab[2])[1] * (Tab[1])[2] * (Tab[0])[0] + (Tab[2])[2] * (Tab[1])[0] * (Tab[0])[1];
+    return sp_sum - sm_sum;
+}
+
+
+//kopiowanie domyslne przez kompilator
+void Macierz::Transpozycja(Macierz & Mc) {
+    Macierz P;
+    P = Mc;
+    for (int i = 0; i < ROZMIAR; i++) {
+        for (int j = 0; j < ROZMIAR; j++) {
+            (Mc[j])[i] = (P[i])[j];
+        }
+    }
+}
+
+Macierz const Macierz::operator+(const Macierz & W)const {
+    Macierz S;
+    for (int i = 0; i < ROZMIAR; i++) {
+        for (int j = 0; j < ROZMIAR; j++) {
+            (S[j])[i] = (Tab[j])[i] + (W[i])[j];
+
+        }
+    }
+    return S;
+}
+
+Macierz const Macierz::operator-(const Macierz & W)const {
+    Macierz S;
+    for (int i = 0; i < ROZMIAR; i++) {
+        for (int j = 0; j < ROZMIAR; j++) {
+            (S[j])[i] = (Tab[j])[i] - (W[i])[j];
+
+        }
+    }
+    return S;
+}
+
+
+Macierz const Macierz::operator*(const Macierz & W)const {
+    Macierz S;
+    for (int i = 0; i < ROZMIAR; i++) {
+        for (int j = 0; j < ROZMIAR; j++) {
+            double s = 0;
+            for (int k = 0; k < ROZMIAR; k++) s += (Tab[i])[k] * (W[k])[j];
+            (S[j])[i] = s;
+
+        }
+    }
+    return S;
+}
 
 std::istream & operator >> (std::istream & Strm, Macierz & Mac)
 {
